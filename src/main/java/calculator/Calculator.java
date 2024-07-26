2489b153c566e2f2abdbc071f2a974709aa92291
Robbie <robbie331@>
2024-07-27 00:04:57 +0900

    Level 2 - 4 데이터 삭제 기능

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -22,6 +22,11 @@ public class Calculator {
 
     }
 
+    // 가장 먼저 저장된 데이터 삭제
+    public void removeResult() {
+        list.remove(0);
+    }
+
     public void calculate(int firstNum, int secondNum, char signal) throws DivideException {
         int result = 0; // 사칙연산 결과 값 저장 변수
         switch (signal) {
@@ -47,11 +52,5 @@ public class Calculator {
         // list에 결과 저장
         System.out.println("결과 : " + result);
         list.add(result);
-
-        Scanner sc = new Scanner(System.in);
-
-        // 가장 먼저 연산된 결과 삭제
-        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력)");
-        if (sc.next().equals("remove")) list.remove(0);
     }
 }
