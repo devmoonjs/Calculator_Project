c8dc0298fb45a680dde24759b6ef3aafbe195a17
Robbie <robbie331@>
2024-07-26 15:32:36 +0900

    Level 2 - 1 & 2. Calculator 클래스 생성 및 Divide 예외 처리 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,6 +8,8 @@ public class App {
         String exit = "";
         List<Integer> list = new ArrayList<>();
 
+        Calculator calculator = new Calculator();
+
         while(!exit.equals("exit")) {
             System.out.println("첫 번째 숫자를 입력하세요 : ");
             int firstNum = sc.nextInt();
@@ -18,33 +20,13 @@ public class App {
             System.out.println("사칙연산 기호를 입력하세요 : ");
             char signal = sc.next().charAt(0);
 
-            int result = 0; // 사칙연산 결과 값 저장 변수
-            switch (signal) {
-                case '+' :
-                    result = firstNum + secondNum;
-                    break;
-
-                case '-' :
-                    result = firstNum - secondNum;
-                    break;
-
-                case '*' :
-                    result = firstNum * secondNum;
-                    break;
-
-                case '/' :
-                    if (secondNum != 0) result = firstNum / secondNum;
-                    else System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
-                    break;
+            // 연산은 Calculator 에서 구현
+            try {
+                list = calculator.calculate(firstNum, secondNum, signal);
+            } catch (DivideException e) {
+                System.out.println(e.getMessage());
             }
 
-            // list에 결과 저장
-            System.out.println("결과 : " + result);
-            list.add(result);
-
-            // 가장 먼저 연산된 결과 삭제
-            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력)");
-            if (sc.next().equals("remove")) list.remove(0);
 
             // 향상된 for 문 활용 저장 결과 출력
             System.out.println("저장된 연산결과를 조회하시겠습니까? (inquiry 입력 시 조회)");
--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,5 +1,52 @@
 package calculator;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+class DivideException extends Exception {
+    // 오버로딩
+    DivideException(String message) {
+        System.out.println(message);
+    }
+}
+
 public class Calculator {
 
+    List<Integer> list = new ArrayList<>();
+
+    public List<Integer> calculate(int firstNum, int secondNum, char signal) throws DivideException {
+        int result = 0; // 사칙연산 결과 값 저장 변수
+        switch (signal) {
+            case '+' :
+                result = firstNum + secondNum;
+                break;
+
+            case '-' :
+                result = firstNum - secondNum;
+                break;
+
+            case '*' :
+                result = firstNum * secondNum;
+                break;
+
+            case '/' :
+                if (secondNum == 0) {
+                    throw new DivideException("0으로 나눌 수 없습니다. ");
+                }
+                break;
+        }
+
+        // list에 결과 저장
+        System.out.println("결과 : " + result);
+        list.add(result);
+
+        Scanner sc = new Scanner(System.in);
+
+        // 가장 먼저 연산된 결과 삭제
+        System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력)");
+        if (sc.next().equals("remove")) list.remove(0);
+
+        return list;
+    }
 }
