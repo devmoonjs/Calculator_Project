c44f08b910e33b2a84199e7805dbf937d9fcc360
Robbie <robbie331@>
2024-07-26 11:47:17 +0900

    Level 1 - 7. 연산 결과 무한 저장 및 결과 제거 기능 추가

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -8,8 +8,7 @@ public class App {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String exit = "";
-        int[] resultArr = new int[resultSize];
-        int submitCount = 0;
+        List<Integer> list = new ArrayList<>();
 
         while(!exit.equals("exit")) {
             System.out.println("첫 번째 숫자를 입력하세요 : ");
@@ -40,14 +39,11 @@ public class App {
                     else System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
                     break;
             }
-            if (submitCount >= 10) { // 결과 저장 배열이 10개를 초과할 경우
-                for (int i = resultArr.length - 1; i > 0; i--) { // 뒤에서 부터 하나씩 제거하며 이동
-                    resultArr[i] = resultArr[i-1];
-                }
-                resultArr[0] = result;
-             } else resultArr[submitCount++] = result; // 결과 저장
-
             System.out.println("결과 : " + result);
+            list.add(result); // list에 결과 저장
+
+            System.out.println("가장 먼저 저장된 연산 결과를 삭제하시겠습니까? (remove 입력)");
+            if (sc.next().equals("remove")) list.remove(0); // 가장 먼저 연산된 결과 삭제
 
             System.out.println("더 계산하시겠습니까 ? (exit 입력 시 종료)");
             exit = sc.next();
