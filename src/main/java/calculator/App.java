229034f10381c79ac27f2e46083e71fd3d5a24ae
Robbie <robbie331@>
2024-07-26 10:49:41 +0900

    Level 1 - 3. switch case 활용 연산 수행 기능

--- src/main/java/calculator/App.java
+++ src/main/java/calculator/App.java
@@ -14,5 +14,28 @@ public class App {
 
         System.out.println("사칙연산 기호를 입력하세요 : ");
         char signal = sc.next().charAt(0);
+
+        int result = 0; // 사칙연산 결과 값 저장 변수
+        switch (signal) {
+            case '+' :
+                result = firstNum + secondNum;
+                break;
+
+            case '-' :
+                result = firstNum - secondNum;
+                break;
+
+            case '*' :
+                result = firstNum * secondNum;
+                break;
+
+            case '/' :
+                if (secondNum != 0) result = firstNum / secondNum;
+                else System.out.println("나눗셈 연산에서 분모(두번째 정수)에 0이 입력될 수 없습니다.");
+                break;
+        }
+
+        System.out.println(result);
+
     }
 }
